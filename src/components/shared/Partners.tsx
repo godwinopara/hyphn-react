b32d6a1e968584f2e30083fc8a7bfbd1c8e5f6f5
Godwin Opara <godwinopara62@yahoo.com>
2024-02-10 05:41:43 +0100

    updated partners

--- src/components/shared/Partners.tsx
+++ src/components/shared/Partners.tsx
@@ -1,14 +1,14 @@
-import harvest from "../../images/hervest.svg";
-import enyata from "../../images/enyata.svg";
-import uba from "../../images/uba.svg";
 import ehealth from "../../images/ehealth.svg";
+import accu from "../../images/accu.svg";
+import accent from "../../images/accent.svg";
+import inbody from "../../images/inbody.svg";
 
 export default function Partners() {
 	return (
 		<section className="max-w-desktop mx-auto flex items-center justify-center px-8 my-8 gap-x-4 md:gap-x-10 md:px-0 xl:gap-x-14">
-			<img src={harvest} alt="hervest logo" className="w-20 md:w-[12%]" />
-			<img src={enyata} alt="enyata logo" className="w-20 md:w-[18%]" />
-			<img src={uba} alt="uba logo" className="w-20 md:w-[12%]" />
+			<img src={inbody} alt="hervest logo" className="w-20 md:w-[12%]" />
+			<img src={accent} alt="enyata logo" className="w-20 md:w-[12%]" />
+			<img src={accu} alt="uba logo" className="w-20 md:w-[18%]" />
 			<img src={ehealth} alt="ehealth logo" className="w-20 md:w-[12%]" />
 		</section>
 	);
